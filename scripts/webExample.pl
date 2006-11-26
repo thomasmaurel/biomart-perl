40b7e5507a8150028070ff3807aa2cdba54c21c8
arek <arek@sanger.ac.uk>
2006-11-26 16:42:47 +0000

    fix

--- scripts/webExample.pl
+++ scripts/webExample.pl
@@ -12,7 +12,7 @@ while (<FH>){
 close(FH);
 
 
-my $path="http://test.biomart.org/cgi-bin/martservice?";
+my $path="http://dev.biomart.org/biomart/martservice?";
 my $request = HTTP::Request->new("POST",$path,HTTP::Headers->new(),'query='.$xml."\n");
 my $ua = LWP::UserAgent->new;
 
