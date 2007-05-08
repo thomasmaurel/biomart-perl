6958a3272462922c6a5ef4a6bb946f21311caeb2
syed <syed@sanger.ac.uk>
2007-05-08 11:26:22 +0000

    ajax working with Safari 2 series. phewwwwwww

--- htdocs/martview/js/martview.js
+++ htdocs/martview/js/martview.js
@@ -1588,90 +1588,90 @@ function check(eltId, state) {
 						|| "an unknown version";
 				this.OS = this.searchString(this.dataOS) || "an unknown OS";
 			},
-   searchString: function (data) {
-	   for (var i=0;i<data.length;i++)	{
-		   var dataString = data[i].string;
-		   var dataProp = data[i].prop;
-		   this.versionSearchString = data[i].versionSearch || data[i].identity;
-		   if (dataString) {
-			   if (dataString.indexOf(data[i].subString) != -1)
-			   return data[i].identity;
-		   }
-		   else if (dataProp)
-		   return data[i].identity;
-	   }
-   },
-   searchVersion: function (dataString) {
-	   var index = dataString.indexOf(this.versionSearchString);
-	   if (index == -1) return;
-	   return parseFloat(dataString.substring(index+this.versionSearchString.length+1));
-   },
-   dataBrowser: [
-   {
-	   string: navigator.userAgent,
-    subString: "Apple",
-    identity: "Safari"
-   },
-   {
-	   prop: window.opera,
-    identity: "Opera"
-   },
-   {
-	   string: navigator.vendor,
-    subString: "iCab",
-    identity: "iCab"
-   },
-   {
-	   string: navigator.vendor,
-    subString: "KDE",
-    identity: "Konqueror"
-   },
-   {
-	   string: navigator.userAgent,
-    subString: "Firefox",
-    identity: "Firefox"
-   },
-   {	// for newer Netscapes (6+)
-	   string: navigator.userAgent,
-    subString: "Netscape",
-    identity: "Netscape"
-   },
-   {
-	   string: navigator.userAgent,
-    subString: "MSIE",
-    identity: "Explorer",
-    versionSearch: "MSIE"
-   },
-   {
-	   string: navigator.userAgent,
-    subString: "Gecko",
-    identity: "Mozilla",
-    versionSearch: "rv"
-   },
-   { 	// for older Netscapes (4-)
-	   string: navigator.userAgent,
-    subString: "Mozilla",
-    identity: "Netscape",
-    versionSearch: "Mozilla"
-   }
-   ],
-   dataOS : [
-   {
-	   string: navigator.platform,
-    subString: "Win",
-    identity: "Windows"
-   },
-   {
-	   string: navigator.platform,
-    subString: "Mac",
-    identity: "Mac"
-   },
-   {
-	   string: navigator.platform,
-    subString: "Linux",
-    identity: "Linux"
-   }
-   ]
+searchString: function (data) {
+	for (var i=0;i<data.length;i++)	{
+		var dataString = data[i].string;
+		var dataProp = data[i].prop;
+		this.versionSearchString = data[i].versionSearch || data[i].identity;
+		if (dataString) {
+			if (dataString.indexOf(data[i].subString) != -1)
+			return data[i].identity;
+		}
+		else if (dataProp)
+		return data[i].identity;
+	}
+},
+searchVersion: function (dataString) {
+	var index = dataString.indexOf(this.versionSearchString);
+	if (index == -1) return;
+	return parseFloat(dataString.substring(index+this.versionSearchString.length+1));
+},
+dataBrowser: [
+{
+	string: navigator.userAgent,
+subString: "Apple",
+identity: "Safari"
+},
+{
+	prop: window.opera,
+identity: "Opera"
+},
+{
+	string: navigator.vendor,
+subString: "iCab",
+identity: "iCab"
+},
+{
+	string: navigator.vendor,
+subString: "KDE",
+identity: "Konqueror"
+},
+{
+	string: navigator.userAgent,
+subString: "Firefox",
+identity: "Firefox"
+},
+{	// for newer Netscapes (6+)
+	string: navigator.userAgent,
+subString: "Netscape",
+identity: "Netscape"
+},
+{
+	string: navigator.userAgent,
+subString: "MSIE",
+identity: "Explorer",
+versionSearch: "MSIE"
+},
+{
+	string: navigator.userAgent,
+subString: "Gecko",
+identity: "Mozilla",
+versionSearch: "rv"
+},
+{ 	// for older Netscapes (4-)
+	string: navigator.userAgent,
+subString: "Mozilla",
+identity: "Netscape",
+versionSearch: "Mozilla"
+}
+],
+dataOS : [
+{
+	string: navigator.platform,
+subString: "Win",
+identity: "Windows"
+},
+{
+	string: navigator.platform,
+subString: "Mac",
+identity: "Mac"
+},
+{
+	string: navigator.platform,
+subString: "Linux",
+identity: "Linux"
+}
+]
 
 		};
 		BrowserDetect.init();
@@ -1977,23 +1977,35 @@ function check(eltId, state) {
 		}
 	}
 
-	function doviaJS()
-	{
-		//alert('in here');
-		var iFrameMe = document.createElement("iframe");
-		iFrameMe.name = 'newIFrame';
-		iFrameMe.style.width = '500px';
-		iFrameMe.style.height = '300px';
-		iFrameMe.style.visibility = 'true';
-		iFrameMe.src = '/help.html';
-		iFrameMe.onload = function ()
+	function doviaJS(val)
+	{		
+		var ua = navigator.userAgent.toLowerCase(); 
+		var versionMinor = parseFloat( ua.substring( ua.lastIndexOf('safari/') + 7 ) );
+		
+		if (versionMinor >= 412)
 		{
-			window.parent.getCountAjax();
-		}
-		//alert('and again');
-		document.mainform.appendChild(iFrameMe);
-		//alert('and again and again ');
-				
+			//alert('Safari 2 series');
+			var iFrameMe = document.createElement("iframe");
+			iFrameMe.name = 'newIFrame';
+			iFrameMe.style.width = '500px';
+			iFrameMe.style.height = '300px';
+			//iFrameMe.style.visibility = 'true';
+			iFrameMe.src = '/help.html';
+			iFrameMe.onload = function ()
+			{
+				if (val == 'count')
+				{
+					window.parent.getCountAjax();
+				}
+				if  (val == 'results')
+				{
+					window.parent.getResultsAjax();
+				}			
+			}
+			//alert('and again');
+			document.mainform.appendChild(iFrameMe);
+			//alert('and again and again ');
+		}			
 	}
 
 
